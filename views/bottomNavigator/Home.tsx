3d539078b9b22ef81149384ee15a55e3e1b9f293
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-20 16:16:20 +0700

    Refactor SVG icon functions and add new icons

--- views/bottomNavigator/Home.tsx
+++ views/bottomNavigator/Home.tsx
@@ -3,6 +3,8 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBarCustom';
+import {backIcon, doubleSaveIcon} from '../../assets/svgXml';
+import {vh, vw} from '../../services/styleSheet';
 
 const Home = () => {
   useStatusBar('white');
@@ -15,10 +17,12 @@ const Home = () => {
 
 const Header: React.FC = () => {
   return (
-    <View>
+    <View style={styles.headercontainer}>
+      {backIcon(vw(7), vw(7), '#98A2B3')}
       <Text style={{color: '#4E5BA6', fontSize: 20, fontWeight: '600'}}>
         Storm Forecast
       </Text>
+      {doubleSaveIcon(vw(7), vw(7), '#98A2B3')}
     </View>
   );
 };
@@ -27,4 +31,10 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: 'white'},
+  headercontainer: {
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: vh(2),
+  },
 });
