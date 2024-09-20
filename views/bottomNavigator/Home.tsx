362033ffb0e2d34b83e5ba967bea642f55f25294
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-20 16:14:18 +0700

    Refactor SVG icon functions and add new icons
    This commit refactors the SVG icon functions in the `svgXml.tsx` file and adds two new icons: `backIcon` and `doubleSaveIcon`. The functions now accept optional parameters for width, height, and color. The icons are defined using SVG XML code and are used for various components throughout the application.
    
    The commit also includes a new component called `Header` in the `Home.tsx` file. This component displays a text header with the title "Storm Forecast" in a blue color. The `useStatusBarCustom.ts` file is added, which contains a custom hook `useStatusBar` that sets the background color of the status bar.
    
    These changes improve the code structure and enhance the visual appearance of the application.

--- views/bottomNavigator/Home.tsx
+++ views/bottomNavigator/Home.tsx
@@ -1,19 +1,30 @@
+/* eslint-disable react-native/no-inline-styles */
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import useStatusBar from '../../services/useStatusBarCustom';
 
 const Home = () => {
+  useStatusBar('white');
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text>Home</Text>
-      </View>
+      <Header />
     </SafeAreaView>
+  );
+};
+
+const Header: React.FC = () => {
+  return (
+    <View>
+      <Text style={{color: '#4E5BA6', fontSize: 20, fontWeight: '600'}}>
+        Storm Forecast
+      </Text>
+    </View>
   );
 };
 
 export default Home;
 
 const styles = StyleSheet.create({
-  container: {flex: 1},
+  container: {flex: 1, backgroundColor: 'white'},
 });
