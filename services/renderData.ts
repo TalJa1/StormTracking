748267309159f80e2e8af717540035daebc5ac29
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-21 17:58:58 +0700

    Refactor PopUpComponent to display storm level data

--- services/renderData.ts
+++ services/renderData.ts
@@ -52,3 +52,166 @@ export const AdditionLocaiton = [
     lad: 20.81717,
   },
 ];
+
+export const StormLevelData = [
+  {
+    title: 'Bão Cấp 1 (Tốc độ gió từ 74-95 km/h)',
+    characteristic:
+      'Gió mạnh vừa, có thể gây ra thiệt hại nhẹ cho cây cối, nhà cửa không kiên cố và hệ thống điện.',
+    preventiveMeasures: [
+      'Kiểm tra nhà cửa: Đảm bảo các cửa sổ, cửa chính được đóng chặt, gia cố lại mái nhà nếu cần.',
+      'Dọn dẹp sân vườn: Loại bỏ các vật dụng nhẹ hoặc cố định chúng để tránh bị gió cuốn đi.',
+      'Chuẩn bị đồ dùng cần thiết: Sạc đầy điện thoại, pin dự phòng, và chuẩn bị đèn pin, nước uống, thực phẩm dự trữ.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 2 (Tốc độ gió từ 96-110 km/h)',
+    characteristic:
+      'Gió mạnh hơn, có khả năng làm bật gốc cây nhỏ, hư hại một số mái nhà và gây mất điện cục bộ.',
+    preventiveMeasures: [
+      'Hạn chế ra ngoài: Chỉ ra ngoài khi thực sự cần thiết; tránh gần các vật cao và yếu như cây cối, cột điện.',
+      'Cập nhật thông tin thường xuyên: Theo dõi các bản tin dự báo bão từ cơ quan khí tượng để biết hướng di chuyển và diễn biến của bão.',
+      'Chuẩn bị nơi trú ẩn an toàn: Đảm bảo có kế hoạch di chuyển đến nơi trú ẩn an toàn nếu cần thiết.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 3 (Tốc độ gió từ 111-130 km/h)',
+    characteristic:
+      'Gió rất mạnh, có thể gây thiệt hại đáng kể cho cây cối, nhà cửa và hệ thống điện.',
+    preventiveMeasures: [
+      'Gia cố nhà cửa: Đảm bảo tất cả các cửa sổ và cửa chính được gia cố chắc chắn.',
+      'Di chuyển đến nơi an toàn: Nếu nhà không kiên cố, hãy di chuyển đến nơi trú ẩn an toàn.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 4 (Tốc độ gió từ 131-155 km/h)',
+    characteristic:
+      'Gió cực mạnh, có thể gây thiệt hại nghiêm trọng cho nhà cửa, cây cối và hệ thống điện.',
+    preventiveMeasures: [
+      'Di tản: Nếu sống trong khu vực dễ bị ảnh hưởng, hãy di tản đến nơi an toàn.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+      'Theo dõi thông tin: Liên tục cập nhật thông tin từ cơ quan chức năng.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 5 (Tốc độ gió từ 156-180 km/h)',
+    characteristic:
+      'Gió cực kỳ mạnh, có thể gây thiệt hại rất nghiêm trọng cho nhà cửa, cây cối và hệ thống điện.',
+    preventiveMeasures: [
+      'Di tản ngay lập tức: Nếu sống trong khu vực dễ bị ảnh hưởng, hãy di tản ngay lập tức.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+      'Theo dõi thông tin: Liên tục cập nhật thông tin từ cơ quan chức năng.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 6 (Tốc độ gió từ 181-210 km/h)',
+    characteristic:
+      'Gió cực kỳ mạnh, có thể gây thiệt hại rất nghiêm trọng cho nhà cửa, cây cối và hệ thống điện.',
+    preventiveMeasures: [
+      'Di tản ngay lập tức: Nếu sống trong khu vực dễ bị ảnh hưởng, hãy di tản ngay lập tức.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+      'Theo dõi thông tin: Liên tục cập nhật thông tin từ cơ quan chức năng.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 7 (Tốc độ gió từ 211-250 km/h)',
+    characteristic:
+      'Gió cực kỳ mạnh, có thể gây thiệt hại rất nghiêm trọng cho nhà cửa, cây cối và hệ thống điện.',
+    preventiveMeasures: [
+      'Di tản ngay lập tức: Nếu sống trong khu vực dễ bị ảnh hưởng, hãy di tản ngay lập tức.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+      'Theo dõi thông tin: Liên tục cập nhật thông tin từ cơ quan chức năng.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 8 (Tốc độ gió từ 251-300 km/h)',
+    characteristic:
+      'Gió cực kỳ mạnh, có thể gây thiệt hại rất nghiêm trọng cho nhà cửa, cây cối và hệ thống điện.',
+    preventiveMeasures: [
+      'Di tản ngay lập tức: Nếu sống trong khu vực dễ bị ảnh hưởng, hãy di tản ngay lập tức.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+      'Theo dõi thông tin: Liên tục cập nhật thông tin từ cơ quan chức năng.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 9 (Tốc độ gió từ 301-350 km/h)',
+    characteristic:
+      'Gió cực kỳ mạnh, có thể gây thiệt hại rất nghiêm trọng cho nhà cửa, cây cối và hệ thống điện.',
+    preventiveMeasures: [
+      'Di tản ngay lập tức: Nếu sống trong khu vực dễ bị ảnh hưởng, hãy di tản ngay lập tức.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+      'Theo dõi thông tin: Liên tục cập nhật thông tin từ cơ quan chức năng.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 10 (Tốc độ gió từ 351-400 km/h)',
+    characteristic:
+      'Gió cực kỳ mạnh, có thể gây thiệt hại rất nghiêm trọng cho nhà cửa, cây cối và hệ thống điện.',
+    preventiveMeasures: [
+      'Di tản ngay lập tức: Nếu sống trong khu vực dễ bị ảnh hưởng, hãy di tản ngay lập tức.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+      'Theo dõi thông tin: Liên tục cập nhật thông tin từ cơ quan chức năng.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 11 (Tốc độ gió từ 401-450 km/h)',
+    characteristic:
+      'Gió cực kỳ mạnh, có thể gây thiệt hại rất nghiêm trọng cho nhà cửa, cây cối và hệ thống điện.',
+    preventiveMeasures: [
+      'Di tản ngay lập tức: Nếu sống trong khu vực dễ bị ảnh hưởng, hãy di tản ngay lập tức.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+      'Theo dõi thông tin: Liên tục cập nhật thông tin từ cơ quan chức năng.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 12 (Tốc độ gió từ 451 km/h trở lên)',
+    characteristic:
+      'Gió cực kỳ mạnh, có thể gây thiệt hại rất nghiêm trọng cho nhà cửa, cây cối và hệ thống điện.',
+    preventiveMeasures: [
+      'Di tản ngay lập tức: Nếu sống trong khu vực dễ bị ảnh hưởng, hãy di tản ngay lập tức.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+      'Theo dõi thông tin: Liên tục cập nhật thông tin từ cơ quan chức năng.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 13 (Tốc độ gió từ 451-500 km/h)',
+    characteristic:
+      'Gió cực kỳ mạnh, có thể gây thiệt hại rất nghiêm trọng cho nhà cửa, cây cối và hệ thống điện.',
+    preventiveMeasures: [
+      'Di tản ngay lập tức: Nếu sống trong khu vực dễ bị ảnh hưởng, hãy di tản ngay lập tức.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+      'Theo dõi thông tin: Liên tục cập nhật thông tin từ cơ quan chức năng.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 14 (Tốc độ gió từ 501-550 km/h)',
+    characteristic:
+      'Gió cực kỳ mạnh, có thể gây thiệt hại rất nghiêm trọng cho nhà cửa, cây cối và hệ thống điện.',
+    preventiveMeasures: [
+      'Di tản ngay lập tức: Nếu sống trong khu vực dễ bị ảnh hưởng, hãy di tản ngay lập tức.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+      'Theo dõi thông tin: Liên tục cập nhật thông tin từ cơ quan chức năng.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 15 (Tốc độ gió từ 551-600 km/h)',
+    characteristic:
+      'Gió cực kỳ mạnh, có thể gây thiệt hại rất nghiêm trọng cho nhà cửa, cây cối và hệ thống điện.',
+    preventiveMeasures: [
+      'Di tản ngay lập tức: Nếu sống trong khu vực dễ bị ảnh hưởng, hãy di tản ngay lập tức.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+      'Theo dõi thông tin: Liên tục cập nhật thông tin từ cơ quan chức năng.',
+    ],
+  },
+  {
+    title: 'Bão Cấp 16 (Tốc độ gió từ 601-650 km/h)',
+    characteristic:
+      'Gió cực kỳ mạnh, có thể gây thiệt hại rất nghiêm trọng cho nhà cửa, cây cối và hệ thống điện.',
+    preventiveMeasures: [
+      'Di tản ngay lập tức: Nếu sống trong khu vực dễ bị ảnh hưởng, hãy di tản ngay lập tức.',
+      'Chuẩn bị đồ dùng cần thiết: Đảm bảo có đủ nước uống, thực phẩm, và các vật dụng cần thiết khác.',
+      'Theo dõi thông tin: Liên tục cập nhật thông tin từ cơ quan chức năng.',
+    ],
+  },
+];
