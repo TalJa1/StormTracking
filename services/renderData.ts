acc8bf09cc8a4182f4329e65554640f55e25ea63
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-21 15:37:07 +0700

    Add location data to renderData.ts

---
+++ services/renderData.ts
@@ -0,0 +1,30 @@
+export const LocationData = [
+  {
+    name: 'Hanoi',
+    description: 'Mưa rào',
+    temperature: 28,
+    long: 105.804817,
+    lad: 21.028511,
+  },
+  {
+    name: 'Tp HCM',
+    description: 'Mưa rải rác',
+    temperature: 26,
+    long: 106.660172,
+    lad: 10.762622,
+  },
+  {
+    name: 'Lào Cai',
+    description: 'Quang mây',
+    temperature: 26,
+    long: 103.975,
+    lad: 22.48056,
+  },
+  {
+    name: 'Hòa Bình',
+    description: 'Nhiều mây',
+    temperature: 26,
+    long: 105.33759,
+    lad: 20.81717,
+  },
+];
