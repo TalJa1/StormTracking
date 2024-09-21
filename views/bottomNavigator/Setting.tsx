e1e96dd0a74bf7927f117337b96ed6e6bf1f734a
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-21 18:38:34 +0700

    Refactor Setting.tsx to add system settings section

--- views/bottomNavigator/Setting.tsx
+++ views/bottomNavigator/Setting.tsx
@@ -10,7 +10,7 @@ import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBarCustom';
 import {vh, vw} from '../../services/styleSheet';
-import {HelpCenter, OtherInfor, Self} from '../../services/renderData';
+import {HelpCenter, OtherInfor, Self, SystemSetting} from '../../services/renderData';
 import {nextIcon} from '../../assets/svgXml';
 import {RenderLayoutInterface} from '../../services/typeProps';
 
@@ -32,6 +32,27 @@ const Setting = () => {
         </View>
         <View style={{paddingHorizontal: vw(5)}}>
           <RenderLayout title="Cá nhân" renderData={Self} />
+          <View>
+            <Text style={styles.title}>Cài đặt hệ thống</Text>
+            {SystemSetting.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} style={styles.renderContainer}>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      columnGap: vw(2),
+                    }}>
+                    {item.icon}
+                    <Text style={{color: '#1D2939', fontSize: 16}}>
+                      {item.title}
+                    </Text>
+                  </View>
+                  {nextIcon(vw(7), vw(7), '#98A2B3')}
+                </TouchableOpacity>
+              );
+            })}
+          </View>
           <RenderLayout title="Trung tâm trợ giúp" renderData={HelpCenter} />
           <RenderLayout title="Thông tin khác" renderData={OtherInfor} />
         </View>
